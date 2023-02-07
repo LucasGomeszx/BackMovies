6fb701e42efa560cbb1793a52f6f249e82275e9f
Lucas Gomes <lgomeszx@gmail.com>
2023-02-07 19:03:27 -0300

    Feature: Criando tela detalhes do ator

---
+++ BackMovies/BackMovies/ActorDetails/ActorDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ActorDetailsViewController.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 07/02/23.
+//
+
+import UIKit
+
+class ActorDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ BackMovies/BackMovies/MovieDetails/MovieDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsViewController.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 07/02/23.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
