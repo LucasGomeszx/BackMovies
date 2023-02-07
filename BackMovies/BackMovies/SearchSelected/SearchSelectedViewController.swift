05e56296cc7624386dffd87c47f7198fb83804ba
Lucas Gomes <lgomeszx@gmail.com>
2023-02-07 19:19:29 -0300

    Feature: Criando tela PesquisaSelecionada

---
+++ BackMovies/BackMovies/SearchSelected/SearchSelectedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSelectedViewController.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 07/02/23.
+//
+
+import UIKit
+
+class SearchSelectedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
